9ca743d236933dcfa53fed8821dc2682cb13c8ac
redperiabras <redperiabras@gmail.com>
2017-04-24 12:14:17 +0800

    Adding scoresheet with slight glitches on the design

--- judging/judge_1/index.php
+++ judging/judge_1/index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Scoring System</title>	
+		<title>Scoring System</title>
+		<!--
+		<link rel="icon" href="images/icon.ico" type="image/png" sizes="32x32">
+		-->
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
+		
 		<!-- Bootstrap -->
 		<link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
 		<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"> -->
@@ -26,66 +29,190 @@
 
 		<link rel="stylesheet" href="../../assets/css/scoring-system.css">
 
-		<link rel="stylesheet" href="../../assets/css/style.css">
+		<!-- <link rel="stylesheet" href="../assets/css/style.css"> -->
 
 		<script src="../../assets/js/angular.min.js"></script>
 
-		<script src="./scoring-system.js"></script>	
-	  
+		<script src="../../assets/js/sheet.js"></script>	
 	</head>
-
-	<body class="container" ng-app="score-app" ng-controller="score-ctrl" ng-init="score = 0">
-
-
-		<br/><br /><br />
-		
-		<h1 id="unionbank">Unionbank Ultimate Pitch</h1>
-
-		
-		<div ng-repeat="team in teams">
-		
-			<div class="team row" ng-hide="hasActive">
-				<div class="col-md-2">
-					<img src="../../assets/images/REDLOGO.png" class="team-logo" />
-				</div>
-				<div class="col-md-6">
-					<h1> {{ team.team_name }} {{team.total}}</h1>
-				</div>
-				<div class="col-md-4 text-center">
-					<button class="btn btn-lg btnSet" ng-click="setScore(team)">Set Score</button>
+	<body class="bgimg">
+		<header class="container">
+			<div class="row text-center">
+				<!-- Name of Event -->
+				<div class="row">
+					<h4 id="event-name">NAME OF EVENT</h4>
 				</div>
 			</div>
-
+		</header>
+		
+		<section class="container" ng-app="scoring-sheet" ng-controller="sheet-ctrl" ng-init="init()">
+		
+			<div id="events-record">
 			
-			<div class="setscore row" ng-show="team.isActive">
-				<div class="col-md-12">
-					<button class="btn btn-lg" ng-click="closeTeam(team)">x</button>
-				</div>
-				
-				<br /><br /><br /><br />
-				
-				<div class="col-md-2">
-					<img src="../../assets/images/REDLOGO.png" class="team-logo" />
-				</div>
-				
-				<div class="col-md-10">
-					<h1>Super App<h1>
-					<h3>The best app in existence</h3>
-					<br /><br /><br />
-				</div>
-				
-				<hr />
+				<div class="text-center" ng-repeat="team in teams"">
 					
-				<div class="criteria row" ng-repeat="criteria in team.criteria">
-					<div class="criteria-name col-md-offset-2 col-md-4">
-						<h1>{{criteria.name}}</h1>
+					<div ng-hide="activeNow" style="padding: 2em;">
+						<button class="btn team-btn" ng-click="setScore(team)">
+							<div class="row">
+								<div class="col-md-3 team-logo">
+									<img src="../../assets/images/logo.gif"/>
+								</div>
+								<div class="col-md-6">
+									<h3>{{team.name}}</h3>
+								</div>
+								<div class="col-md-3 team-score">
+									<h1>{{team.total}} %</h1>
+								</div>
+							</div>
+						</button>
 					</div>
 					
-					<div class="criteria-score col-md-6">
-						<input type="number" max="{{criteria.max}}" ng-model="criteria.score" ng-change="updateScore(team)"/>
+					<div ng-show="team.isActive">
+					
+						<div class="row">
+							
+							<div class="col-md-6 text-left">
+								<button class="btn btn-nav" ng-click="closeTeam(team)">View All Teams</button>
+							</div>
+							
+							<div class="col-md-6 text-right">
+								<button class="btn btn-nav" ng-click="prevTeam(team)">Previous</button>
+								<button class="btn btn-nav" ng-click="nextTeam(team)">Next</button>
+							</div>
+						
+						</div>
+					
+						<div class="row">
+						
+							<div class="col-md-6 sheet-div">
+							
+								<div id="team-desc">
+								
+									<div class="row team-desc">
+										<h3>{{team.name}}</h3>
+									</div>
+									
+									<div class="row team-desc">
+										<p>TEAM MEMBERS</p>
+									</div>
+									
+									<div class="row team-desc">
+										<h4>PROJECT NAME</h4>
+									</div>
+									
+									<div class="row team-desc">
+										<p>PROJECT DESCRIPTION</p>
+									</div>
+								
+								</div>
+								
+							</div>
+						
+							<div class="col-md-6 sheet-div">
+							
+								<div class="row team-desc">
+									<h3>SCORING SHEET</h3>
+								</div>
+								
+								<!-- Criteria Header -->
+								<div class="row">
+								
+									<div class="col-md-6">
+										<h4>CRITERIA</h4>
+									</div>
+									
+									<div class="col-md-2">
+									
+									</div>
+									
+									<div class="col-md-4">
+										<h4>SCORE</h4>
+									</div>
+									
+								</div>
+								
+								<hr/>
+								
+								
+								<!-- Criteria and Score -->
+								<div class="row" id="criteria" ng-repeat="criteria in team.criteria">
+										
+										<div class="col-md-1">
+											<h5>{{criteria.criteria_id}}</h5>
+										</div>
+										
+										<div class="col-md-7 text-left">
+											<h4>{{criteria.name}}</h4>
+											<small><i>{{criteria.description}}</i></small>
+										</div>
+												
+										<div class="col-md-4">
+											<h4><input type="number" class="text-right" name="criteria-{{criteria.criteria_id}}" style="width: 4em;" ng-model="criteria.score" ng-change="updateScore(team)"/><span> / {{criteria.weight}}</span></h4>
+										</div>
+										
+										<hr>
+									
+								</div>
+								
+								<hr/>
+								
+								<!-- Total Score -->
+								<div class="row">
+								
+									<div class="col-md-6">
+										<h4>TOTAL</h4>
+									</div>
+									
+									<div class="col-md-2">
+									
+									</div>
+									
+									<div class="col-md-4">
+										<h4>{{team.total}} %</h4>
+									</div>
+									
+								</div>
+								
+								<br/><br/>
+								
+								<div class="row text-center container">
+								
+									<div class="col-md-2">
+									</div>
+								
+									<div class="col-md-6">
+										<button class="btn btn-lg btn-primary submit-score">SUBMIT</button>
+									</div>
+									
+									<div class="col-md-2">
+									</div>
+									
+								</div>
+								
+								<br/>
+								
+							</div>
+						
+						</div>
+					
 					</div>
+					
 				</div>
+				<!--
+				<div id="done-style" class="text-center">
+					<button class="btn btn-primary">DONE</button>
+				</div>
+				-->
 			</div>
-		</div>
+			
+		</section>
+
+		<footer class="container text-center">
+		
+			<p><small>Powered by </small><strong>RED Wizard Events Management</strong> &copy; 2017</p>
+			
+		</footer>
+		
 	</body>
+
 </html>
