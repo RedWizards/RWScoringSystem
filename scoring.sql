67441edeb99f306a1ad2dac80bd74291d6a1ac65
redperiabras <redperiabras@gmail.com>
2017-04-27 17:14:31 +0800

    database latest

--- scoring.sql
+++ scoring.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 24, 2017 at 04:33 AM
+-- Generation Time: Apr 27, 2017 at 11:12 AM
 -- Server version: 5.6.26-log
 -- PHP Version: 7.0.4
 
@@ -141,7 +141,7 @@ INTO @sql FROM
     
 	SET @sql = (
 		CONCAT('
-			SELECT project_id, project_name, team_id, team_name,', @sql ,'
+			SELECT project_id, project_name, team_id, team_name,', @sql ,', (SELECT SUM(score) FROM scores WHERE judge_id = ',in_judge_id,' AND project_id = raw_scores.project_id) AS Total
 			FROM
 				(
 					SELECT DISTINCT
@@ -163,10 +163,12 @@ INTO @sql FROM
 							AND t.team_id = p.team_id
 							AND c.event_id = p.event_id
 							AND s.criteria_id = c.criteria_id
-					ORDER BY project_id , team_id, criteria_desc
+					ORDER BY t.team_name
 				) AS raw_scores
 			GROUP BY
 				project_id
+			ORDER BY
+				team_name
 		')
 	);
 
@@ -184,8 +186,12 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `register_team` (IN `in_team_name` V
     );
 END$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_score` (IN `in_score_id` INT, IN `in_score` INT)  BEGIN
+	UPDATE scores SET score = in_score WHERE score_id = in_score_id;
+END$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `view_criteria` (IN `in_event_id` INT(11))  BEGIN
-	SELECT criteria_id, criteria_desc, criteria_weight
+	SELECT criteria_id, criteria_desc, criteria_longdesc, criteria_weight
     FROM criteria
     WHERE event_id = in_event_id;
 END$$
@@ -201,29 +207,26 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `view_members` (IN `in_team_id` INT(
 	WHERE team_id = in_team_id;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `view_score` (IN `in_project_id` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `view_score` (IN `in_project_id` INT, IN `in_judge_id` INT, IN `in_criteria_id` INT)  BEGIN
 	SELECT
-		c.criteria_desc,
-        s.score
+		score_id,
+		score
 	FROM
-		scores s, 
-		project p,
-        event e,
-        criteria c
-	WHERE s.project_id = in_project_id
-			AND p.project_id = s.project_id
-            AND e.event_id = p.event_id
-            AND c.event_id = p.event_id
-            AND s.criteria_id = c.criteria_id;
+		scores
+        
+	WHERE project_id = in_project_id
+			AND judge_id = in_judge_id
+            AND criteria_id = in_criteria_id;
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `view_teams` (IN `in_event_id` INT(11))  BEGIN
-	SELECT t.team_id, t.team_name, p.project_name, p.project_type, p.short_desc, p.long_desc
+	SELECT t.team_id, t.team_name, p.project_id, p.project_name, p.project_type, p.short_desc, p.long_desc
     FROM team t,
 		project p
     WHERE 
 		p.event_id = in_event_id
-    AND t.team_id = p.team_id;
+    AND t.team_id = p.team_id
+    ORDER BY t.team_name;
 END$$
 
 DELIMITER ;
@@ -238,18 +241,19 @@ CREATE TABLE `criteria` (
   `criteria_id` int(11) NOT NULL,
   `event_id` int(11) NOT NULL,
   `criteria_desc` varchar(160) DEFAULT NULL,
-  `criteria_weight` int(11) DEFAULT NULL
+  `criteria_weight` int(11) DEFAULT NULL,
+  `criteria_longdesc` varchar(800) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `criteria`
 --
 
-INSERT INTO `criteria` (`criteria_id`, `event_id`, `criteria_desc`, `criteria_weight`) VALUES
-(2, 2, 'Innovation', 25),
-(3, 2, 'Technical Difficulty', 25),
-(4, 2, 'Business Impact', 25),
-(5, 2, 'Demo', 25);
+INSERT INTO `criteria` (`criteria_id`, `event_id`, `criteria_desc`, `criteria_weight`, `criteria_longdesc`) VALUES
+(1, 1, 'Scalability and Impact', 25, 'To what extent can the project be replicated or adapted by the bank and  different sectors Can this app be used by its target audience?'),
+(2, 1, 'Execution and Design', 25, 'Do they have a prototype? How functional is the technical demo? Design matters! Does the  project easy to use?'),
+(3, 1, 'Business Model', 25, 'How can they make it a successful  business? What customer segments they have defined and who are the early adopters? What are the (potential) revenue / cost models?'),
+(4, 1, 'Project Validation', 25, 'Did they test the market Are they solving real problems? What is the value preposition?');
 
 -- --------------------------------------------------------
 
@@ -261,7 +265,7 @@ CREATE TABLE `event` (
   `event_id` int(11) NOT NULL,
   `event_name` varchar(45) DEFAULT NULL,
   `event_host` varchar(45) DEFAULT NULL,
-  `event_desc` varchar(45) DEFAULT NULL,
+  `event_desc` varchar(800) DEFAULT NULL,
   `event_date` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -270,8 +274,7 @@ CREATE TABLE `event` (
 --
 
 INSERT INTO `event` (`event_id`, `event_name`, `event_host`, `event_desc`, `event_date`) VALUES
-(2, 'Red Wizard OJT', NULL, 'Sample Event', '2017-04-19 01:18:30'),
-(3, 'UHAC FINAL PITCHING', 'UNIONBANK', 'UHAC Series Grand Champion', '2017-04-19 12:46:02');
+(1, 'U:HAC Ultimate Pitching ', 'Unionbank', NULL, '2017-04-28 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -290,10 +293,21 @@ CREATE TABLE `judge` (
 --
 
 INSERT INTO `judge` (`judge_id`, `event_id`, `judge_name`) VALUES
-(2, 2, 'Redentor Periabras'),
-(4, 2, 'Elaine Cedillo'),
-(5, 2, 'Denise Pantig'),
-(6, 2, 'Tonichi Dela Cruz');
+(1, 1, 'Christian Cimbracruz'),
+(2, 1, 'Prince Julius Hari'),
+(3, 1, 'Tonichi Paul Dela Cruz'),
+(4, 1, 'Red Periabras'),
+(5, 1, 'Francis Olivo'),
+(6, 1, 'Ariel Conde'),
+(7, 1, 'Jedidiah Garcia'),
+(8, 1, 'Eadrian Marzan'),
+(9, 1, 'Clint Santos'),
+(10, 1, 'Jordan Bolinas'),
+(11, 1, 'Jem Zubiri'),
+(12, 1, 'Ian Briñosa'),
+(13, 1, 'Paul Sablan'),
+(14, 1, 'Jay Gecarane'),
+(15, 1, 'Joshua Castañeda');
 
 -- --------------------------------------------------------
 
@@ -315,8 +329,14 @@ CREATE TABLE `participants` (
 --
 
 INSERT INTO `participants` (`participant_id`, `team_id`, `participant_firstName`, `participant_lastName`, `participant_email`, `participant_contactNo`) VALUES
-(1, 2, 'Redentor', 'Periabras', 'redperiabras@gmail.com', '09278572198'),
-(5, 2, 'Christian', 'Cimbracruz', 'christiancimbra@gmail.com', '09093291283');
+(1, 1, 'Chris', 'Militante', NULL, NULL),
+(2, 1, 'Patrick', 'Woogue', NULL, NULL),
+(3, 1, 'Gabriel Andrew', 'Pineda', NULL, NULL),
+(4, 3, 'Jelo Nicole', 'Javier', NULL, NULL),
+(5, 3, 'Jayson', 'Abilar', NULL, NULL),
+(6, 3, 'Carlo', 'Jumagdao', NULL, NULL),
+(7, 3, 'John Paul', 'Escala', NULL, NULL),
+(8, 3, 'Rafael', 'Desuyo', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -339,11 +359,22 @@ CREATE TABLE `project` (
 --
 
 INSERT INTO `project` (`project_id`, `event_id`, `team_id`, `project_name`, `project_type`, `short_desc`, `long_desc`) VALUES
-(2, 2, 2, 'PLDT Bot', 'Messenger Bot', 'meh', 'bah blah'),
-(3, 2, 5, 'Scoring System', 'Web', 'haha', 'ohhh'),
-(4, 2, 6, 'Music App', 'Web', 'lalala', 'hahaha'),
-(5, 2, 5, 'Leslee', 'Web', '***', '****'),
-(6, 2, 6, 'Kuya TEam', 'web', '***', '****');
+(1, 1, 1, 'LaurelEye', 'Web Application', 'An Uber of Tutorials', 'LaurelEye is an web-based platform where people can find other people willing to educate them in a vast array  of topics from academic subjects to arts and crafts.'),
+(2, 1, 2, 'Chibot', 'Mobile Application', 'Virtual Reality Listing app for Foreclosed Properties', 'The app focuses on Real Estate and Cars. The app can be deployed via major App stores (Apple App Store, Google Playstore). App also includes a VR walk-through of a Virtual Bank to familiarize customers of the different things you can do in a branch and also to promote the bank''s ongoing promos or marketing campaigns. The property viewer will contain a "dibs/I''m Interested" button that submits the user''s contact info, making it easier for the bank to contact new leads. This can help the bank increase its market reach faster. As for the users, this decreases guess work and logistics by cutting the need to go to the actual site just by experiencing the interactive photo-spheres and real-time 3D spaces that eventually speed up buyers''  decision to buy or not.'),
+(3, 1, 3, 'Intern', 'Web and Mobile Application', 'Amobile recording of traffic violations with banking payment. ', 'This app maintains the history of traffic violation records of a certain driver that has a account on the app while also recording the traffic enforcer info who arrested him/her. This app also provides digital banking for the driver that has been arrested. He/she can pay to Unionbank for his violation/s.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `remarks`
+--
+
+CREATE TABLE `remarks` (
+  `remarks_id` int(11) NOT NULL,
+  `judge_id` int(11) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  `remark` varchar(800) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -356,7 +387,7 @@ CREATE TABLE `scores` (
   `judge_id` int(11) DEFAULT NULL,
   `criteria_id` int(11) DEFAULT NULL,
   `project_id` int(11) DEFAULT NULL,
-  `score` double DEFAULT NULL
+  `score` double DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -364,18 +395,186 @@ CREATE TABLE `scores` (
 --
 
 INSERT INTO `scores` (`score_id`, `judge_id`, `criteria_id`, `project_id`, `score`) VALUES
-(5, 2, 3, 2, 20),
-(6, 2, 4, 2, 15),
-(7, 2, 5, 2, 10),
-(8, 2, 2, 2, 15),
-(9, 6, 2, 3, 25),
-(10, 6, 4, 3, 25),
-(11, 6, 5, 3, 25),
-(12, 6, 3, 3, 25),
-(14, 2, 2, 5, 10),
-(15, 2, 3, 5, 5),
-(16, 2, 4, 5, 3),
-(18, 2, 5, 5, 20);
+(1, 1, 1, 1, 2),
+(2, 1, 2, 1, 5),
+(3, 1, 3, 1, 2),
+(4, 1, 4, 1, 12),
+(5, 2, 1, 1, 0),
+(6, 2, 2, 1, 0),
+(7, 2, 3, 1, 0),
+(8, 2, 4, 1, 0),
+(9, 3, 1, 1, 0),
+(10, 3, 2, 1, 0),
+(11, 3, 3, 1, 0),
+(12, 3, 4, 1, 0),
+(13, 4, 1, 1, 0),
+(14, 4, 2, 1, 0),
+(15, 4, 3, 1, 0),
+(16, 4, 4, 1, 0),
+(17, 5, 1, 1, 0),
+(18, 5, 2, 1, 0),
+(19, 5, 3, 1, 0),
+(20, 5, 4, 1, 0),
+(21, 6, 1, 1, 0),
+(22, 6, 2, 1, 0),
+(23, 6, 3, 1, 0),
+(24, 6, 4, 1, 0),
+(25, 7, 1, 1, 0),
+(26, 7, 2, 1, 0),
+(27, 7, 3, 1, 0),
+(28, 7, 4, 1, 0),
+(29, 8, 1, 1, 0),
+(30, 8, 2, 1, 0),
+(31, 8, 3, 1, 0),
+(32, 8, 4, 1, 0),
+(33, 9, 1, 1, 0),
+(34, 9, 2, 1, 0),
+(35, 9, 3, 1, 0),
+(36, 9, 4, 1, 0),
+(37, 10, 1, 1, 0),
+(38, 10, 2, 1, 0),
+(39, 10, 3, 1, 0),
+(40, 10, 4, 1, 0),
+(41, 11, 1, 1, 0),
+(42, 11, 2, 1, 0),
+(43, 11, 3, 1, 0),
+(44, 11, 4, 1, 0),
+(45, 12, 1, 1, 0),
+(46, 12, 2, 1, 0),
+(47, 12, 3, 1, 0),
+(48, 12, 4, 1, 0),
+(49, 13, 1, 1, 0),
+(50, 13, 2, 1, 0),
+(51, 13, 3, 1, 0),
+(52, 13, 4, 1, 0),
+(53, 14, 1, 1, 0),
+(54, 14, 2, 1, 0),
+(55, 14, 3, 1, 0),
+(56, 14, 4, 1, 0),
+(57, 15, 1, 1, 0),
+(58, 15, 2, 1, 0),
+(59, 15, 3, 1, 0),
+(60, 15, 4, 1, 0),
+(61, 1, 1, 2, 2),
+(62, 1, 2, 2, 12),
+(63, 1, 3, 2, 20),
+(64, 1, 4, 2, 10),
+(65, 2, 1, 2, 0),
+(66, 2, 2, 2, 0),
+(67, 2, 3, 2, 0),
+(68, 2, 4, 2, 1),
+(69, 3, 1, 2, 0),
+(70, 3, 2, 2, 0),
+(71, 3, 3, 2, 0),
+(72, 3, 4, 2, 0),
+(73, 4, 1, 2, 0),
+(74, 4, 2, 2, 0),
+(75, 4, 3, 2, 0),
+(76, 4, 4, 2, 0),
+(77, 5, 1, 2, 0),
+(78, 5, 2, 2, 0),
+(79, 5, 3, 2, 0),
+(80, 5, 4, 2, 0),
+(81, 6, 1, 2, 0),
+(82, 6, 2, 2, 0),
+(83, 6, 3, 2, 0),
+(84, 6, 4, 2, 0),
+(85, 7, 1, 2, 0),
+(86, 7, 2, 2, 0),
+(87, 7, 3, 2, 0),
+(88, 7, 4, 2, 0),
+(89, 8, 1, 2, 0),
+(90, 8, 2, 2, 0),
+(91, 8, 3, 2, 0),
+(92, 8, 4, 2, 0),
+(93, 9, 1, 2, 0),
+(94, 9, 2, 2, 0),
+(95, 9, 3, 2, 0),
+(96, 9, 4, 2, 0),
+(97, 10, 1, 2, 0),
+(98, 10, 2, 2, 0),
+(99, 10, 3, 2, 0),
+(100, 10, 4, 2, 0),
+(101, 11, 1, 2, 0),
+(102, 11, 2, 2, 0),
+(103, 11, 3, 2, 0),
+(104, 11, 4, 2, 0),
+(105, 12, 1, 2, 0),
+(106, 12, 2, 2, 0),
+(107, 12, 3, 2, 0),
+(108, 12, 4, 2, 0),
+(109, 13, 1, 2, 0),
+(110, 13, 2, 2, 0),
+(111, 13, 3, 2, 0),
+(112, 13, 4, 2, 0),
+(113, 14, 1, 2, 0),
+(114, 14, 2, 2, 0),
+(115, 14, 3, 2, 0),
+(116, 14, 4, 2, 0),
+(117, 15, 1, 2, 0),
+(118, 15, 2, 2, 0),
+(119, 15, 3, 2, 0),
+(120, 15, 4, 2, 0),
+(121, 1, 1, 3, 10),
+(122, 1, 2, 3, 10),
+(123, 1, 3, 3, 20),
+(124, 1, 4, 3, 25),
+(125, 2, 1, 3, 0),
+(126, 2, 2, 3, 0),
+(127, 2, 3, 3, 0),
+(128, 2, 4, 3, 0),
+(129, 3, 1, 3, 0),
+(130, 3, 2, 3, 0),
+(131, 3, 3, 3, 0),
+(132, 3, 4, 3, 0),
+(133, 4, 1, 3, 0),
+(134, 4, 2, 3, 0),
+(135, 4, 3, 3, 0),
+(136, 4, 4, 3, 0),
+(137, 5, 1, 3, 0),
+(138, 5, 2, 3, 0),
+(139, 5, 3, 3, 0),
+(140, 5, 4, 3, 0),
+(141, 6, 1, 3, 0),
+(142, 6, 2, 3, 0),
+(143, 6, 3, 3, 0),
+(144, 6, 4, 3, 0),
+(145, 7, 1, 3, 0),
+(146, 7, 2, 3, 0),
+(147, 7, 3, 3, 0),
+(148, 7, 4, 3, 0),
+(149, 8, 1, 3, 0),
+(150, 8, 2, 3, 0),
+(151, 8, 3, 3, 0),
+(152, 8, 4, 3, 0),
+(153, 9, 1, 3, 0),
+(154, 9, 2, 3, 0),
+(155, 9, 3, 3, 0),
+(156, 9, 4, 3, 0),
+(157, 10, 1, 3, 0),
+(158, 10, 2, 3, 0),
+(159, 10, 3, 3, 0),
+(160, 10, 4, 3, 0),
+(161, 11, 1, 3, 0),
+(162, 11, 2, 3, 0),
+(163, 11, 3, 3, 0),
+(164, 11, 4, 3, 0),
+(165, 12, 1, 3, 0),
+(166, 12, 2, 3, 0),
+(167, 12, 3, 3, 0),
+(168, 12, 4, 3, 0),
+(169, 13, 1, 3, 0),
+(170, 13, 2, 3, 0),
+(171, 13, 3, 3, 0),
+(172, 13, 4, 3, 0),
+(173, 14, 1, 3, 0),
+(174, 14, 2, 3, 0),
+(175, 14, 3, 3, 0),
+(176, 14, 4, 3, 0),
+(177, 15, 1, 3, 0),
+(178, 15, 2, 3, 0),
+(179, 15, 3, 3, 0),
+(180, 15, 4, 3, 0);
 
 -- --------------------------------------------------------
 
@@ -393,10 +592,9 @@ CREATE TABLE `team` (
 --
 
 INSERT INTO `team` (`team_id`, `team_name`) VALUES
-(2, 'Harambeats'),
-(5, 'Team Mamba'),
-(6, 'WhiteCloak'),
-(7, 'Team Arandia');
+(1, 'LaurelEye'),
+(2, 'Chibot'),
+(3, 'Intern');
 
 --
 -- Indexes for dumped tables
@@ -438,6 +636,14 @@ ALTER TABLE `project`
   ADD KEY `project_event_idx` (`event_id`);
 
 --
+-- Indexes for table `remarks`
+--
+ALTER TABLE `remarks`
+  ADD PRIMARY KEY (`remarks_id`),
+  ADD KEY `remarks_judge_idx` (`judge_id`),
+  ADD KEY `remarks_project_idx` (`project_id`);
+
+--
 -- Indexes for table `scores`
 --
 ALTER TABLE `scores`
@@ -460,37 +666,42 @@ ALTER TABLE `team`
 -- AUTO_INCREMENT for table `criteria`
 --
 ALTER TABLE `criteria`
-  MODIFY `criteria_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `criteria_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `judge`
 --
 ALTER TABLE `judge`
-  MODIFY `judge_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `judge_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `participants`
 --
 ALTER TABLE `participants`
-  MODIFY `participant_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `participant_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `remarks`
+--
+ALTER TABLE `remarks`
+  MODIFY `remarks_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `scores`
 --
 ALTER TABLE `scores`
-  MODIFY `score_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `score_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=181;
 --
 -- AUTO_INCREMENT for table `team`
 --
 ALTER TABLE `team`
-  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Constraints for dumped tables
 --
@@ -519,6 +730,13 @@ ALTER TABLE `participants`
 ALTER TABLE `project`
   ADD CONSTRAINT `project_event` FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `project_team` FOREIGN KEY (`team_id`) REFERENCES `team` (`team_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `remarks`
+--
+ALTER TABLE `remarks`
+  ADD CONSTRAINT `remarks_judge` FOREIGN KEY (`judge_id`) REFERENCES `judge` (`judge_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `remarks_project` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `scores`
